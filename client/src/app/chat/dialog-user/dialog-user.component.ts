509115f1b97fa73a9842f16f101530688602e92c
menyuscraft <menyet@index.hu>
2018-03-31 15:25:45 +0200

    fixed an issue when dialog buttons are not visible

--- client/src/app/chat/dialog-user/dialog-user.component.ts
+++ client/src/app/chat/dialog-user/dialog-user.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { FormControl, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { DialogUserType } from './dialog-user-type';
 
 @Component({
   selector: 'tcc-dialog-user',
@@ -10,6 +11,7 @@ import { FormControl, Validators } from '@angular/forms';
 export class DialogUserComponent implements OnInit {
   usernameFormControl = new FormControl('', [Validators.required]);
   previousUsername: string;
+  dialogUT = DialogUserType;
 
   constructor(public dialogRef: MatDialogRef<DialogUserComponent>,
     @Inject(MAT_DIALOG_DATA) public params: any) {
